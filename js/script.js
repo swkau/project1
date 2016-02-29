92c90621ceffaff76410244c18d167b3d571f26c
Kau Si Wei <Imm@Kaus-MacBook.local>
2016-03-01 00:41:58 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,162 @@
+$( document ).ready(function() {
+
+var position = [0,0];
+var pTurn = 1;
+
+$( "#roll" ).click(function() {
+  var result = rollDice();
+  calculate(result);
+  checkActivity();
+  if(position[0]>=100||position[1]>=100){
+    alert('Player ' + pTurn + ' is the winner!');
+    return;
+    }
+  changeTurn(result);
+});
+
+function rollDice(){
+    var double = 0;
+    var d1 = Math.floor(Math.random() * 7);
+    var d2 = Math.floor(Math.random() * 7);
+    var diceTotal = d1 + d2;
+    $('#activity').html('');
+    $('#die1').text(d1);
+    $('#die2').text(d2);
+    $('#status').html("You rolled " + diceTotal + ".");
+    if(d1 == d2){
+        $('#status').html("2 x " + d1 + "!! Rolled doubles! Free turn!!");
+        double = 1;
+    }
+    return [diceTotal, double];
+}
+
+function calculate(result){
+    var oldposition = position[(pTurn-1)];
+    position[(pTurn-1)] = position[(pTurn-1)] + result[0];
+    move(oldposition);
+}
+
+function move(oldposition, activityType) {
+    if(position[(pTurn-1)]>100) { position[(pTurn-1)] = 100; }
+    $('#p'+pTurn+'position').text("player " + pTurn + " position: " + position[(pTurn-1)]);
+    $('#'+oldposition).removeClass("p"+pTurn+" p12");
+    if(position[0]===position[1]) { $('#'+position[(pTurn-1)]).addClass("p"+pTurn+" p12"); }
+    else { $('#'+position[(pTurn-1)]).addClass("p"+pTurn); }
+}
+
+function checkActivity(result){
+    var oldposition = position[(pTurn-1)];
+    var mapping = {
+    1:38, 4:14, 9:31, 17:7, 21:42, 28:38, 51:67, 54:34, 62:19, 64:60, 71:91, 80:100, 87:24, 93:73, 95:75, 98:79 
+    };
+    if(mapping[oldposition]) {
+    position[(pTurn-1)] = mapping[oldposition];
+    if(position[(pTurn-1)]>oldposition) {
+    move(oldposition,"ladder");
+    $('#activity').html('You climb up a ladder!');
+    }
+    else if(position[(pTurn-1)]<oldposition) {
+    move(oldposition,"snake");
+    $('#activity').html('You get eaten by a snake!');
+    }
+    }
+
+ /*   switch(position[(pTurn-1)]) {
+        case 1:
+        position[(pTurn-1)] = 38;
+        move(oldposition,"ladder");
+        $('#activity').html('You climb up a ladder!');
+        break;
+        case 4:
+        position[(pTurn-1)] = 14;
+        move(oldposition,"ladder");
+        $('#activity').html('You climb up a ladder!');
+        break;
+        case 9:
+        position[(pTurn-1)] = 31;
+        move(oldposition,"ladder");
+        $('#activity').html('You climb up a ladder!');
+        break;
+        case 17:
+        position[(pTurn-1)] = 7;
+        move(oldposition,"snake");
+        $('#activity').html('You get eaten by a snake!');
+        break;
+        case 21:
+        position[(pTurn-1)] = 42;
+        move(oldposition,"ladder");
+        $('#activity').html('You climb up a ladder!');
+        break;
+        case 28:
+        position[(pTurn-1)] = 38;
+        move(oldposition,"ladder");
+        $('#activity').html('You climb up a ladder!');
+        break;
+        case 51:
+        position[(pTurn-1)] = 67;
+        move(oldposition,"ladder");
+        $('#activity').html('You climb up a ladder!');
+        break;
+        case 54:
+        position[(pTurn-1)] = 34;
+        move(oldposition,"snake");
+        $('#activity').html('You get eaten by a snake!');
+        break;
+        case 62:
+        position[(pTurn-1)] = 19;
+        move(oldposition,"snake");
+        $('#activity').html('You get eaten by a snake!');
+        break;
+        case 64:
+        position[(pTurn-1)] = 60;
+        move(oldposition,"snake");
+        $('#activity').html('You get eaten by a snake!');
+        break;
+        case 71:
+        position[(pTurn-1)] = 91;
+        move(oldposition,"ladder");
+        $('#activity').html('You climb up a ladder!');
+        break;
+        case 80:
+        position[(pTurn-1)] = 100;
+        move(oldposition,"ladder");
+        $('#activity').html('You climb up a ladder!');
+        break;
+        case 87:
+        position[(pTurn-1)] = 24;
+        move(oldposition,"snake");
+        $('#activity').html('You get eaten by a snake!');
+        break;
+        case 93:
+        position[(pTurn-1)] = 73;
+        move(oldposition,"snake");
+        $('#activity').html('You get eaten by a snake!');
+        break;
+        case 95:
+        position[(pTurn-1)] = 75;
+        move(oldposition,"snake");
+        $('#activity').html('You get eaten by a snake!');
+        break;
+        case 98:
+        position[(pTurn-1)] = 79;
+        move(oldposition,"snake");
+        $('#activity').html('You get eaten by a snake!');
+        break;
+        default:        
+    } */
+}
+
+function changeTurn(result){
+    if(result[1]===0) {
+        if(pTurn===1) {
+            pTurn = 2;
+            $('#turn').text("Player 2's turn!");
+        }
+        else if (pTurn===2) {
+            pTurn = 1;
+            $('#turn').text("Player 1's turn!");
+        }
+    }
+}
+
+});
